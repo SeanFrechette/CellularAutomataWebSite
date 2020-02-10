514cf117a53ce704a066920b8a2b1b8d6ddf3e74
SeanFrechette <55244335+SeanFrechette@users.noreply.github.com>
2020-02-10 18:14:02 -0500

    Add files via upload

--- behavior.js
+++ behavior.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	var thumbnailElement = document.getElementById("smart_thumbnail");
 	thumbnailElement.addEventListener("click", function() {
-  //alert("test");
+
 });
+});
+$(function(){
+  	var mainNav = $("nav");
+  	alert("Hello There");
 });
